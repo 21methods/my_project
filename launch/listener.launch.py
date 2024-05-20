f34e4a1ebdafe778bb0d47a101945bd16008f911
21methods <monojoshua2@gmail.com>
2024-05-20 14:01:51 +0300

    first commit

---
+++ launch/listener.launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from lanch_ros.actions import Node
+
+
+def generate_lanch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_node_py',
+            executable='listener'
+        )
+    ])
